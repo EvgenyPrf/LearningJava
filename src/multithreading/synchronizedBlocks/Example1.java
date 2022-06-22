4d7e8ff538262aebd62fa09af0f4a0cc7fd7157b
Evgeniy Parfenyuk <info.jacktiger@gmail.com>
2022-06-22 21:38:37 +0300

    new changes to the 6 line in class Example1

--- src/multithreading/synchronizedBlocks/Example1.java
+++ src/multithreading/synchronizedBlocks/Example1.java
@@ -3,5 +3,6 @@ package multithreading.synchronizedBlocks;
 public class Example1 {
   public static void main(String[] args){
     System.out.println("hello");
+    System.out.println("hello from idea");
   }
 }
