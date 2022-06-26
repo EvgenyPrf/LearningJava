6df387c6f7b38dc6401f59e7d067b271bb2c8070
Evgeny <parf-evgenij@yandex.ru>
2022-06-26 17:34:12 +0300

    finished the lesson about thread pool and executor service (part two)

--- src/multithreading/threadPoolAndExecutorService/ThreadPoolEx1.java
+++ src/multithreading/threadPoolAndExecutorService/ThreadPoolEx1.java
@@ -1,6 +1,7 @@
+package multithreading.threadPoolAndExecutorService;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolEx1 {
   public static void main(String[] args) throws InterruptedException {
---
+++ src/multithreading/threadPoolAndExecutorService/ThreadPoolEx2.java
@@ -0,0 +1,52 @@
+package multithreading.threadPoolAndExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolEx2 {
+    public static void main(String[] args) throws InterruptedException {
+        ScheduledExecutorService scheduledExecutorService =
+                Executors.newScheduledThreadPool(1);
+//        for (int i = 0; i < 10; i++) {
+//            scheduledExecutorService.execute(new RunnableIml2());
+//        }
+        /*метод schedule выполнит наши задания через пределённый период времени
+         * */
+//        scheduledExecutorService.schedule(new RunnableIml2(), 3, TimeUnit.SECONDS);
+
+//планиурет задачу для перидическогоо выполнения: в примере ниже задача начнет выполняться через три секунды и продолжит свое
+        //выполнение с перидичностью 1 секунда. Одна секунда в данном случае проходит с момента начала первого задания
+        //до момента начала второго задания
+//        scheduledExecutorService.scheduleAtFixedRate(new RunnableIml2(), 3, 1, TimeUnit.SECONDS);
+
+        //scheduleWithFixedDelay такой же метод, как и scheduleAtFixedRate, но разница в том, что одна секунда пройдет
+        //с момента окончания первого задания до момента начала второго задания.
+        scheduledExecutorService.scheduleWithFixedDelay(new RunnableIml2(), 3, 1, TimeUnit.SECONDS);
+        Thread.sleep(20000);
+        scheduledExecutorService.shutdown();
+//кешированный тред пул. Метод newCachedThreadPool создает тред пул, который будет создавать новые потоки по надобности.
+        /* Thread1 <- task1
+        Thread2 <- task2
+         если Thread1 свбодился, а в execotorService поступает новый таск, то поток может быть переопределён.
+         Он возьмет на себя выполнение данноого таска, и новый поток не будет создан.
+         Thread1 <- task3
+         Если поток освободился, но в течение 60-ти сек он не получает новый таск, то он будет уничтожен.
+        * */
+        ExecutorService executorService = Executors.newCachedThreadPool();
+    }
+}
+
+class RunnableIml2 implements Runnable {
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " begins work");
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println(Thread.currentThread().getName() + " ends work");
+
+    }
+}
