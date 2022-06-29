49ae2306030ba6eb92143a39283fd2b19f6204ca
Evgeny Parfenyuk <107951931+EvgenyPrf@users.noreply.github.com>
2022-06-29 14:54:11 +0000

    begin lesson about exchanger

---
+++ src/multithreading/exchanger/ExchangerEx.java
@@ -0,0 +1,16 @@
+public class ExchangerEx {
+  /*рассмотрим реализацию exchange между потоками на примере игры камень/ножницы/бумага.
+
+*/
+  public static void main(String [] args){
+    
+  }
+}
+
+enum Action{
+  ROCK, PAPER, SCISSORS;
+}
+class BestFriend extends Thread{
+  private String name;
+  
+}
