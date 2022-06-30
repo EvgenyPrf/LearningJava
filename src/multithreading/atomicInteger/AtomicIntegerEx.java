054702ac86d8077c602dc578082e4f72fb1a6941
Evgeny Parfenyuk <107951931+EvgenyPrf@users.noreply.github.com>
2022-06-30 06:55:58 +0000

    finished the lesson about AtomicInteger

---
+++ src/multithreading/atomicInteger/AtomicIntegerEx.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerEx {
+/* Вспоминаем пример, при котором у нас возникало состояние data race, при котором
+два потока меняют одну и ту же переменную без синхронизации.Их нужно было синхронизировать, так как операция инкремент не атомарная. Ранее эту проблему мы решали с помощью synchronized (этот процесс не быстрый, так как проходит сначала блокировка монитора, потом его разблокировка и так далее), но в java есть класс AtomicInteger, в котором все операции происходят атомарно.
+*/
+//static int counter = 0;
+
+  static AtomicInteger counter = new AtomicInteger();
+  
+   public static void increment(){
+    // counter++;
+     // counter.incrementAndGet();
+     counter.addAndGet(5);
+   }
+  
+  public static void main(String []args) throws InterruptedException {
+    Thread thread1 = new Thread(new MyRunnableImpl());
+    Thread thread2 = new Thread(new MyRunnableImpl());
+    thread1.start();
+    thread2.start();
+    thread1.join();
+    thread2.join();
+    System.out.println(counter);
+    
+  }
+}
+class MyRunnableImpl implements Runnable{
+  public void run(){
+    for(int i = 0; i < 100; i++){
+      AtomicIntegerEx.increment();
+    }
+  }
+}
